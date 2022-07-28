c948509704081b9da5505d8819ed8fe565781463
TheBoyWhoLivedd <ayesigwar@gmail.com>
2022-07-28 06:38:35 +0300

    Fetching Videos from Sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,34 @@
+import axios from "axios";
 import type { NextPage } from "next";
+import { Video } from "../types";
 
-const Home: NextPage = () => {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+interface IProps {
+    videos: Video[];
+}
+
+const Home = ({ videos }: IProps) => {
+    console.log(videos);
+    return (
+        <div>
+            {videos.length ? (
+                videos.map((video: Video) => (
+                    <VideoCard post={video} key={video._id} />
+                ))
+            ) : (
+                <NoResults text={"No Videos"} />
+            )}
+        </div>
+    );
+};
+
+export const getServerSideProps = async () => {
+    const { data } = await axios.get(`http://localhost:3000/api/post`);
+
+    return {
+        props: {
+            videos: data,
+        },
+    };
 };
 
 export default Home;
